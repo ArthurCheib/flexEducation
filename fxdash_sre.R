fc0e95109117c24a30508dd592f93dbeeba997a9
Arthur Cheib <arthursilvacheib@gmail.com>
2018-12-17 08:55:09 -0200

    Final version:
    Removing useless lines;
    Finalizing querying

--- fxdash_sre.R
+++ fxdash_sre.R
@@ -1,33 +1,34 @@
 library(googledrive)
-library(RODBC)
-library(rmarkdown)
+library(dbConnect)
+library(RMariaDB)
 library(tidyverse)
 library(knitr)
-library(readxl)
+library(rstudioapi)
 
-#Conversao da data no R para a data em formato numérico do Excel (contagem em dias):
-data_br_hoje <- format(Sys.Date(), format = "%d-%m-%Y")
-data_br_last5 <- format(Sys.Date() - 5, format = "%d-%m-%Y")
+#Conversão da data no R para a data em formato numérico do Excel (contagem em dias):
+data_hoje <- Sys.Date()
+data_last5 <- (Sys.Date() - 5)
 
 # Conectando com o MySQL:
+con <- dbConnect(RMariaDB::MariaDB(),
+                 dbname = "db_ngi",
+                 host = "10.23.185.10",
+                 port = 3306,
+                 user = "arthur_cheib",
+                 password = rstudioapi::askForPassword("Database password"))
+
 qry_01 <- paste0("SELECT * ", 
-                "FROM base ",
+                "FROM TBL_MATRICULA ",
                 "WHERE data ",
-                "BETWEEN ", data_br_last5, " AND ", data_br_hoje, ";")
+                "BETWEEN ", "'",data_last5, "'", " AND ", "'", data_hoje, "'", ";")
 
-df_mat_ent <- sqlQuery(ch, qry_01)
-
-df_mat_ent <- df_mat_ent[, -14]
-
-# Alterar o formato da coluna de Data início período e Data término período
-df_mat_ent$DATA <- as.Date(df_mat_ent$DATA)
-
-# Alterando nomes das datasets para R format:
-real_names <- c("SRE", "COD_MUNICIPIO", "MUNICIPIO", "COD_ESCOLA", "ESCOLA", "ENDERECO", "NIVEL", "ETAPA",
-                "TIPO_TURMA", "TURNO", "ALUNOS_MATRICULADOS", "ALUNOS_ENTURMADOS", "DATA")
-colnames(df_mat_ent) <- real_names
+data <- dbSendQuery(con, qry_01)
+df_mat_ent <- dbFetch(data)
 
 # RENDERIZATION
 rmarkdown::render(input = paste0(getwd(), "/relatorio_regional.Rmd"),
        output_file = "relatorio.html",
        output_dir = getwd())
+
+dbClearResult(data)
+dbDisconnect(con)
