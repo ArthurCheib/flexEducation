540b746e4fe549987600d44c07a1ebb45f4ec25f
Arthur Cheib <arthursilvacheib@gmail.com>
2018-12-20 14:02:12 -0200

    Last fix

--- fxdash_sre.R
+++ fxdash_sre.R
@@ -8,7 +8,7 @@ library(rstudioapi)
 
 #Conversão da data no R para a data em formato numérico do Excel (contagem em dias):
 data_hoje <- Sys.Date()
-data_last5 <- (Sys.Date() - 5)
+data_last7 <- (Sys.Date() - 7)
 data_padrao_encerramento <- as.Date("2018-12-10")
 
 # Conectando com o MySQL:
@@ -20,30 +20,30 @@ con <- dbConnect(RMariaDB::MariaDB(),
                  password = rstudioapi::askForPassword("Database password"))
 
 #### Query para df matrícula e enturmação:
-qry_01 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, QT_ALUNO_MATRICULADO, QT_ALUNO_ENTURMADO, DATA ", 
+qry_01 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, ETAPA, QT_ALUNO_MATRICULADO, QT_ALUNO_ENTURMADO, DATA ", 
                 "FROM TBL_MATRICULA ",
                 "WHERE data ",
-                "BETWEEN ", "'",data_last5, "'", " AND ", "'", data_hoje, "'", ";")
+                "BETWEEN ", "'",data_last7, "'", " AND ", "'", data_hoje, "'", ";")
 
 data_mt <- dbSendQuery(con, qry_01)
 df_mat_ent <- dbFetch(data_mt)
 dbClearResult(data_mt)
 
 #### Query para df criação de turmas:
-qry_02 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, QT_TURMA_PA, QT_TURMA_CRIADA, QT_TURMA_AUTORIZADA, DATA ", 
+qry_02 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, ETAPA, QT_TURMA_PA, QT_TURMA_CRIADA, QT_TURMA_AUTORIZADA, DATA ", 
                  "FROM TBL_CRIACAO ",
                  "WHERE data ",
-                 "BETWEEN ", "'",data_last5, "'", " AND ", "'", data_hoje, "'", ";")
+                 "BETWEEN ", "'",data_last7, "'", " AND ", "'", data_hoje, "'", ";")
 
 data_cr <- dbSendQuery(con, qry_02)
 df_cri_aut <- dbFetch(data_cr)
 dbClearResult(data_cr)
 
 #### Query para df encerramento:
-qry_03 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, ETAPA, QT_ALUNO_ENTURMADO_ATIVO, QT_ALUNO_ENCERRADO, DATA ", 
+qry_03 <- paste0("SELECT SRE, COD_ESCOLA, ESCOLA, NIVEL, ETAPA, TURMA, QT_ALUNO_ENTURMADO_ATIVO, QT_ALUNO_ENCERRADO, DATA ", 
                  "FROM TBL_ENCERRAMENTO ",
                  "WHERE data = ", "'",data_padrao_encerramento, "'", " OR data ",
-                 "BETWEEN ", "'",data_last5, "'", " AND ", "'", data_hoje, "'", ";")
+                 "BETWEEN ", "'",data_last7, "'", " AND ", "'", data_hoje, "'", ";")
 
 data_enc <- dbSendQuery(con, qry_03)
 df_encer <- dbFetch(data_enc)
